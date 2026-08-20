b082280ac3cfccbd78e8852dbaf2bb37b3af4e27
Onyedika Christopher <cosimon.tech@gmail.com>
2026-08-20 18:11:21 +0100

    firebase

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
   output: "export",
 
   images: {
+    unoptimized: true,
+
     remotePatterns: [
       {
         protocol: "https",
